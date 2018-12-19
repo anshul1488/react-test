25b0767a39194d3267b1baea9f5d0c39359a3ce7
anshul1488 <anshul_ie@icloud.com>
2018-12-19 07:52:45 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import "babel-polyfill";
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/SearchBar';
@@ -6,33 +6,50 @@ import ListView from './components/ListView';
 import SidePanel from './components/SidePanel';
 
 class MemberSelfAdminTab extends React.Component {
-  state = { selectedRow: null};
+    constructor(props) {
+        super(props);
+        this.state = {
+            displaySearchBar: true,
+            displayListView: true,
+            displaySidePanel: true,
+            searchResults: null,
+            selectedRowIndex: null
+        };    
+    }
 
-  getSelectedRow = (selectedRowInListView) => {
-    this.setState({ selectedRow: selectedRowInListView });
-  }
+    getSearchResults = (results) => {
+        this.setState({ searchResults: results });
+    }
 
-  render() {
-    return (
-      <div className="ui container">
-        <div className="ui grid">
-				  <div className="sixteen wide column">
-            <SearchBar />
-            <br />
-          </div>
-          <div className="six wide column">
-            <ListView getSelectedRow={this.getSelectedRow} />
-					  <br />
-				  </div>
-				
-				  <div className="ten wide column">
-            <SidePanel selectedRow={this.state.selectedRow} />
-					  <br />
-				  </div>
-        </div>
-		  </div>
-    );
-  }
+    getSelectedRowIndex = (selectedRowIndexInListView) => {
+        this.setState({ selectedRowIndex: selectedRowIndexInListView });
+    }
+
+    render() {  
+        return (
+            <div className="ui container">
+                <div className="ui grid">
+                    { this.state.displaySearchBar &&
+				    <div className="sixteen wide column">
+                        <SearchBar getSearchResults={ this.getSearchResults } />
+                        <br />
+                    </div> }
+                    { this.state.displayListView &&
+                    <div className="six wide column">
+                        <ListView searchResults={ this.state.searchResults }
+                            getSelectedRowIndex={ this.getSelectedRowIndex } />
+				        <br />
+                    </div> }
+                    { this.state.displaySidePanel &&
+				    <div className="ten wide column">
+                        <SidePanel searchResults={ this.state.searchResults } 
+                            selectedRowIndex={ this.state.selectedRowIndex } />
+					    <br />
+                    </div> }
+                </div>
+		    </div>
+        );
+    }
 }
 
 ReactDOM.render(
